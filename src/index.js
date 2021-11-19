78d5fab69daa2cd8b11c6230fac816eecc951439
George Cruz <91498529+gcruz63@users.noreply.github.com>
2021-11-19 13:51:11 -0600

    Revert "chapter 8"

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Repairs } from './components/Repairs';
-import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Repairs />
-    </BrowserRouter>
+    <Repairs />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
