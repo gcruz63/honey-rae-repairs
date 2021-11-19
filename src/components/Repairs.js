0abacca522662aea7b1310ca807188fc95a50356
gcruz.63 <gcruzthethird@gmail.com>
2021-11-19 15:13:31 -0600

    applications was deleted its now added

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,21 +1,16 @@
-import { CustomerList } from "./customers/customersList"
-import { EmployeeList } from "./employeeList/employeeList"
-import { TicketList } from "./serviceTickets/serviceTickets"
+import React from "react"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import "./Repairs.css"
+
 export const Repairs = () => {
 
     return (
         <>
+          <NavBar />
           <h1>Honey Rae's Repair Shop</h1>
-         <div>
-          <h2>Customer List</h2>
-          <CustomerList/>
-         </div>
-         <h2>Employees</h2>
-          <EmployeeList/>
-          <div>
-            <h2>Service Tickets</h2>
-            <TicketList/>
-          </div>
+         
+         <ApplicationViews />
          </>
     )
 }
